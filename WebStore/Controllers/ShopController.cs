feadea6b6c0beefa5a054451de6e7064c943d18c
Tarxos <wolfmanr@mail.ru>
2020-05-27 21:24:11 +0300

    add Shop

---
+++ WebStore/Controllers/ShopController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class ShopController : Controller
+    {
+        public IActionResult Products => View();
+        public IActionResult ProductDetails => View();
+        public IActionResult Checkout => View();
+        public IActionResult Cart => View();
+    }
+}
