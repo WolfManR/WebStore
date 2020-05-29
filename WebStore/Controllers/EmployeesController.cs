0d2df09368ad4258aa8a9e80a38cca0f2eea4b58
Tarxos <wolfmanr@mail.ru>
2020-05-29 13:54:05 +0300

    add Error 404 and start rework Emloyees behavior

--- WebStore/Controllers/EmployeesController.cs
+++ WebStore/Controllers/EmployeesController.cs
@@ -23,5 +23,36 @@ namespace WebStore.Controllers
             if (employee is null) return NotFound();
             return View(employee);
         }
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            employees.Add(employee);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null) return RedirectToAction(nameof(Index));
+            var employee = employees.First(x => Equals(x.Id, id));
+            return employee != null ? (IActionResult)View(employee) : NotFound();
+        }
+        [HttpPost]
+        public IActionResult Edit(int Id, Employee employee)
+        {
+            if (Id == employee.Id) return null;
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Delete() => View();
+
+        [HttpPost]
+        public IActionResult Delete(Employee employee)
+        {
+            employees.Remove(employee);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
---
+++ WebStore/Infrastructure/Services/InMemoryEmployeesDataService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Models;
+
+namespace WebStore.Infrastructure.Services
+{
+    public class InMemoryEmployeesDataService : IEmployeesData
+    {
+        private readonly List<Employee> Employees = TestData.Employees;
+
+        public IEnumerable<Employee> GetAll() => Employees;
+
+        public Employee GetById(int id) => Employees.FirstOrDefault(e => e.Id == id);
+
+        public int Add(Employee Employee)
+        {
+            _ = Employee ?? throw new ArgumentNullException(nameof(Employee));
+
+            if (Employees.Contains(Employee)) return Employee.Id;
+
+            Employee.Id = Employees.Count == 0 ? 1 : Employees.Max(e => e.Id) + 1;
+            Employees.Add(Employee);
+            return Employee.Id;
+        }
+
+        public void Edit(Employee Employee)
+        {
+            _ = Employee ?? throw new ArgumentNullException(nameof(Employee));
+
+            if (Employees.Contains(Employee)) return;
+
+            var db_item = GetById(Employee.Id);
+
+            db_item.FirstName = Employee.FirstName;
+            db_item.Surname = Employee.Surname;
+            db_item.Patronymic = Employee.Patronymic;
+            db_item.Age = Employee.Age;
+        }
+
+        public bool Delete(int id)
+        {
+            var db_item = GetById(id);
+            if (db_item is null) return false;
+
+            return Employees.Remove(db_item);
+        }
+
+        public void SaveChanges() { }
+    }
+}
