dedcb5f765ef55c34d1215808113e72390360eb2
Tarxos <wolfmanr@mail.ru>
2020-06-03 23:07:08 +0300

    Blog Data Service

---
+++ WebStore/Infrastructure/Interfaces/IBlogDataService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+using WebStore.Domain.Entities;
+
+namespace WebStore.Infrastructure.Interfaces
+{
+    interface IBlogDataService
+    {
+        IEnumerable<BlogPost> GetAll();
+        BlogPost GetById(int id);
+    }
+}
---
+++ WebStore/Infrastructure/Services/InMemoryBlogDataService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using WebStore.Domain.Entities;
+using WebStore.Infrastructure.Interfaces;
+
+namespace WebStore.Infrastructure.Services
+{
+    public class InMemoryBlogDataService : IBlogDataService
+    {
+        public IEnumerable<BlogPost> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public BlogPost GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
