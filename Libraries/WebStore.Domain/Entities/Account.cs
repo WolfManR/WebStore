cc90c565cac9edab45c4b7798477d75e420c533b
Tarxos <wolfmanr@mail.ru>
2020-06-03 22:08:02 +0300

    Blog Entities

---
+++ Libraries/WebStore.Domain/Entities/Account.cs
@@ -0,0 +1,12 @@
+﻿
+using WebStore.Domain.Entities.Base;
+
+namespace WebStore.Domain.Entities
+{
+    public class Account : BaseEntity
+    {
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+        public string AvatarUrl { get; set; }
+    }
+}
---
+++ Libraries/WebStore.Domain/Entities/Comment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using WebStore.Domain.Entities.Base;
+
+namespace WebStore.Domain.Entities
+{
+    public class Comment : BaseEntity
+    {
+        public string Text { get; set; }
+        public int AccountId { get; set; }
+        public DateTime Time { get; set; }
+        public int? ParentCommentId { get; set; }
+    }
+}
