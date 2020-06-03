fca3b312b3b4b0015c32e0e86d9b42f34672dcb0
Tarxos <wolfmanr@mail.ru>
2020-06-03 16:24:31 +0300

    Brands View Component

---
+++ WebStore/Components/BrandsViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System.Collections.Generic;
+using System.Linq;
+
+using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels;
+
+namespace WebStore.Components
+{
+    public class BrandsViewComponent : ViewComponent
+    {
+        private readonly IProductDataService productDataService;
+        public BrandsViewComponent(IProductDataService productDataService)
+        {
+            this.productDataService = productDataService;
+        }
+
+        public IViewComponentResult Invoke() => View(GetBrands());
+
+        private IEnumerable<BrandViewModel> GetBrands() => productDataService.GetBrands()
+               .Select(brand => new BrandViewModel
+               {
+                   Id = brand.Id,
+                   Name = brand.Name,
+                   Order = brand.Order
+               })
+               .OrderBy(brand => brand.Order);
+    }
+}
