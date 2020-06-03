93e87fc9b6bdf366bb38c9095c96fe8576a33cbc
Tarxos <wolfmanr@mail.ru>
2020-06-03 16:38:48 +0300

    Products catalog

--- WebStore/Controllers/ShopController.cs
+++ WebStore/Controllers/ShopController.cs
@@ -1,11 +1,46 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
+using System.Linq;
+
+using WebStore.Domain.Entities;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.ViewModels;
+
 namespace WebStore.Controllers
 {
     public class ShopController : Controller
     {
+        private readonly IProductDataService productDataService;
+        public ShopController(IProductDataService productDataService)
+        {
+            this.productDataService = productDataService;
+        }
         public IActionResult Home() => View();
-        public IActionResult Products() => View();
+        public IActionResult Products(int? SectionId, int? BrandId)
+        {
+            var filter = new ProductFilter
+            {
+                SectionId = SectionId,
+                BrandId = BrandId
+            };
+
+            var products = productDataService.GetProducts(filter);
+
+            return View(new CatalogViewModel
+            {
+                SectionId = SectionId,
+                BrandId = BrandId,
+                Products = products.Select(p => new ProductViewModel
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Order = p.Order,
+                    Price = p.Price,
+                    ImageUrl = p.ImageUrl
+                }).OrderBy(p => p.Order)
+            });
+        }
+
         public IActionResult ProductDetails() => View();
         public IActionResult Checkout() => View();
         public IActionResult Cart() => View();
