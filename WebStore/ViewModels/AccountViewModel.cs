eeeb32d2c38d3ebcd6cd39f54789a867848cd8ef
Tarxos <wolfmanr@mail.ru>
2020-06-04 16:42:03 +0300

    add readonly property Name to AccountViewModel

--- WebStore/ViewModels/AccountViewModel.cs
+++ WebStore/ViewModels/AccountViewModel.cs
@@ -3,8 +3,10 @@
     public class AccountViewModel
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
+        public string Firstname { get; set; }
         public string Surname { get; set; }
         public string AvatarUrl { get; set; }
+
+        public string Name { get => $"{Firstname} {Surname}"; }
     }
 }
