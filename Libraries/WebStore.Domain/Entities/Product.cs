e563884679d5f3ff43f289e98ebf7a1154d3d3bf
Tarxos <wolfmanr@mail.ru>
2020-06-07 14:49:17 +0300

    upgrade Models to use Data Anotations

--- Libraries/WebStore.Domain/Entities/Product.cs
+++ Libraries/WebStore.Domain/Entities/Product.cs
@@ -1,4 +1,7 @@
-﻿using WebStore.Domain.Entities.Base;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using WebStore.Domain.Entities.Base;
 using WebStore.Domain.Entities.Base.Interfaces;
 
 namespace WebStore.Domain.Entities
@@ -7,9 +10,21 @@ namespace WebStore.Domain.Entities
     {
         public int Order { get; set; }
 
-        public int SectionId { get; set; }
-        public int? BrandId { get; set; }
+        [Required]
         public string ImageUrl { get; set; }
+
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+
+
+        public int SectionId { get; set; }
+
+        [ForeignKey(nameof(SectionId))]
+        public virtual Section Section { get; set; }
+
+        public int? BrandId { get; set; }
+
+        [ForeignKey(nameof(BrandId))]
+        public virtual Brand Brand { get; set; }
     }
 }
