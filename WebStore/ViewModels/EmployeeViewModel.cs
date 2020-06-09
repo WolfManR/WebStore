23a124cc47663bc45fceff55b67caf66a5cad137
Tarxos <wolfmanr@mail.ru>
2020-06-09 21:52:41 +0300

    add Employee view model

---
+++ WebStore/ViewModels/EmployeeViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+using WebStore.Domain;
+
+namespace WebStore.ViewModels
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Name not specified, required")]
+        [StringLength(maximumLength: 200, MinimumLength = 2, ErrorMessage = "must be from 2 to 200 characters")]
+        public string Firstname { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Surname not indicated, required")]
+        [StringLength(maximumLength: 200, MinimumLength = 2, ErrorMessage = "must be from 2 to 200 characters")]
+        public string Surname { get; set; }
+        public string AvatarUrl { get; set; }
+
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Age not specified, required")]
+        [Range(18, 75)]
+        public int Age { get; set; }
+
+        public Sex Sex { get; set; }
+        public DateTime Birthday { get; set; }
+
+        public string Name { get => $"{Firstname} {Surname}"; }
+    }
+}
