9b776bceaf3717f226b88c86b017dee406784caf
Tarxos <wolfmanr@mail.ru>
2020-06-16 19:08:15 +0300

    Cart Data Service

--- Libraries/WebStore.Domain/Entities/ProductFilter.cs
+++ Libraries/WebStore.Domain/Entities/ProductFilter.cs
@@ -1,8 +1,11 @@
-﻿namespace WebStore.Domain.Entities
+﻿using System.Collections.Generic;
+
+namespace WebStore.Domain.Entities
 {
     public class ProductFilter
     {
         public int? SectionId { get; set; }
         public int? BrandId { get; set; }
+        public IEnumerable<int> Ids { get; set; }
     }
 }
