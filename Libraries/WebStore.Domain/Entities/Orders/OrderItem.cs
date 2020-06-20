cf508b66e96a16ce4f595d40b52a6368dd0450d2
Tarxos <wolfmanr@mail.ru>
2020-06-20 22:05:52 +0300

    Order entities Models

---
+++ Libraries/WebStore.Domain/Entities/Orders/OrderItem.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using WebStore.Domain.Entities.Base;
+
+namespace WebStore.Domain.Entities.Orders
+{
+    public class OrderItem : BaseEntity
+    {
+        [Required]
+        public Order Order { get; set; }
+        [Required]
+        public Product Product { get; set; }
+        [Column(TypeName ="decimal(18,2)")]
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
