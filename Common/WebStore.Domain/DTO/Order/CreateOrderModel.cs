4fa22d778c8c05f7bc2b5fb1865a5f8c66749ab7
Tarxos <wolfmanr@mail.ru>
2020-06-27 23:27:48 +0300

    Order DTO

---
+++ Common/WebStore.Domain/DTO/Order/CreateOrderModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using WebStore.Domain.ViewModels.Products.Orders;
+
+namespace WebStore.Domain.DTO.Order
+{
+    public class CreateOrderModel
+    {
+        public OrderViewModel Order { get; set; }
+        public List<OrderItemDTO> Items { get; set; }
+    }
+}
