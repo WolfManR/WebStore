5493694cceaf60285a82037c7e23bdc361fb82a9
Tarxos <wolfmanr@mail.ru>
2020-06-28 13:17:47 +0300

    Employees Api Controller

---
+++ Services/WebStore.ServiceHosting/Controllers/EmployeesApiController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using System.Collections.Generic;
+
+using WebStore.Domain.Entities;
+using WebStore.Interfaces;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route(WebApiAddresses.Employees)]
+    [ApiController]
+    public class EmployeesApiController : ControllerBase, IRepo<Employee>
+    {
+        private readonly IRepo<Employee> employeesRepo;
+
+        public EmployeesApiController(IRepo<Employee> employeesDataService) => employeesRepo = employeesDataService;
+
+        [HttpGet]
+        public IEnumerable<Employee> GetAll() => employeesRepo.GetAll();
+
+        [HttpGet("{id}")]
+        public Employee GetById(int id) => employeesRepo.GetById(id);
+
+
+        [HttpPost]
+        public int Add([FromBody] Employee entity)
+        {
+            var id = employeesRepo.Add(entity);
+            SaveChanges();
+            return id;
+        }
+
+        [HttpPut]
+        public void Edit(Employee entity)
+        {
+            employeesRepo.Edit(entity);
+            SaveChanges();
+        }
+
+        [HttpDelete]
+        public bool Delete(int id)
+        {
+            var success = employeesRepo.Delete(id);
+            SaveChanges();
+            return success;
+        }
+
+        public void SaveChanges() => employeesRepo.SaveChanges();
+    }
+}
