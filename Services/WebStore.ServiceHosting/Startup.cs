1e35266d2dbdf8bc61f532000f5a7523963dc986
Tarxos <wolfmanr@mail.ru>
2020-06-30 00:10:15 +0300

    Swagger

--- Services/WebStore.ServiceHosting/Startup.cs
+++ Services/WebStore.ServiceHosting/Startup.cs
@@ -9,7 +9,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 using System;
-
+using System.IO;
+using Microsoft.OpenApi.Models;
 using WebStore.DAL.Context;
 using WebStore.Domain.Entities;
 using WebStore.Domain.Entities.Identity;
@@ -62,6 +63,19 @@ namespace WebStore.ServiceHosting
                 .AddScoped<IRepo<Employee>, BaseRepo<Employee>>()
                 .AddScoped<IProductDataService, SqlProductDataService>()
                 .AddScoped<IOrderDataService, SqlOrderDataService>();
+
+            services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc("v1", new OpenApiInfo { Title = "WebStore.API", Version = "v1" });
+
+                const string webDomainXml = "WebStore.Domain.xml";
+                const string webApiXml = "WebStore.ServiceHosting.xml";
+                const string debugPath = "bin/debug/netcoreapp3.1";
+
+                opt.IncludeXmlComments(webApiXml);
+                if (File.Exists(webDomainXml)) opt.IncludeXmlComments(webDomainXml);
+                else if (File.Exists(Path.Combine(debugPath, webDomainXml))) opt.IncludeXmlComments(Path.Combine(debugPath, webDomainXml));
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, WebStoreDBInitializer db)
@@ -76,6 +90,14 @@ namespace WebStore.ServiceHosting
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(opt =>
+            {
+                opt.SwaggerEndpoint("/swagger/v1/swagger.json", "WebStore.API");
+                opt.RoutePrefix = string.Empty;
+            });
 
             app.UseEndpoints(endpoints =>
             {
