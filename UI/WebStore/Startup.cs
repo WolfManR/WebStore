5a82b31ef5005c550c3efad01e7ab6b75d2dc197
Tarxos <wolfmanr@mail.ru>
2020-07-03 16:37:35 +0300

    WebStore UI log4net injection

--- UI/WebStore/Startup.cs
+++ UI/WebStore/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 using System;
 
@@ -18,6 +19,7 @@ using WebStore.Domain.Entities;
 using WebStore.Domain.Entities.Identity;
 using WebStore.Interfaces.Services;
 using WebStore.Interfaces.TestApi;
+using WebStore.Logger.Log4Net;
 using WebStore.Services.Profiles;
 using WebStore.Services.Services.InCookies;
 using WebStore.Services.Services.InMemory;
@@ -98,8 +100,10 @@ namespace WebStore
                 .AddSingleton<IRepo<BlogPost>, InMemoryBlogDataService>();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory log)
         {
+            log.AddLog4Net();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
