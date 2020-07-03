82d5f642033601b72a2e3e7b740af5eeecf44536
Tarxos <wolfmanr@mail.ru>
2020-07-03 15:55:55 +0300

    log4net classes

---
+++ Common/WebStore.Logger/Log4Net/Log4NetExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace WebStore.Logger.Log4Net
+{
+    public static class Log4NetExtensions
+    {
+        public static ILoggerFactory AddLog4Net(this ILoggerFactory factory, string configurationFile = "log4net.config")
+        {
+            if (!Path.IsPathRooted(configurationFile))
+            {
+                var assembly = Assembly.GetEntryAssembly() ?? throw new InvalidOperationException("The assembly with the entry point to the application is not defined");
+                var dir = Path.GetDirectoryName(assembly.Location) ?? throw new InvalidOperationException("Failed to determine the path to the executive assembly directory");
+                configurationFile = Path.Combine(dir, configurationFile);
+            }
+
+            factory.AddProvider(new Log4NetLoggerProvider(configurationFile));
+
+            return factory;
+        }
+    }
+}
