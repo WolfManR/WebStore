cc629acc874a459c6a744dca8f611fa70c537b33
Tarxos <wolfmanr@mail.ru>
2020-07-06 23:18:44 +0300

    Contact & Errors controllers base tests

---
+++ Tests/WebStore.Tests/Controllers/ContactControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using WebStore.Controllers;
+
+using Assert = Xunit.Assert;
+
+namespace WebStore.Tests.Controllers
+{
+    [TestClass]
+    public class ContactControllerTests
+    {
+        [TestMethod]
+        public void ContactUs_Returns_View()
+        {
+            var controller = new ContactController();
+
+            var result = controller.Index();
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
