f890cad8f3580b8ee089709e85d841c808eafda5
Tarxos <wolfmanr@mail.ru>
2020-07-10 18:32:31 +0300

    BreadCrumbs

---
+++ Common/WebStore.Domain/ViewModels/BreadCrumbViewModel.cs
@@ -0,0 +1,12 @@
+﻿using WebStore.Domain.DTO.Products;
+using WebStore.Domain.Entities;
+
+namespace WebStore.Domain.ViewModels
+{
+    public class BreadCrumbViewModel
+    {
+        public Section Section { get; set; }
+        public BrandDTO Brand { get; set; }
+        public string Product { get; set; }
+    }
+}
---
+++ UI/WebStore/Components/BreadCrumbsViewComponent.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.ViewModels;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Components
+{
+    public class BreadCrumbsViewComponent:ViewComponent
+    {
+        private readonly IProductDataService productDataService;
+
+        public BreadCrumbsViewComponent(IProductDataService productDataService) => this.productDataService = productDataService;
+
+        public IViewComponentResult Invoke()
+        {
+            var model = new BreadCrumbViewModel();
+
+            if (int.TryParse(Request.Query["SectionId"].ToString(), out var sectionId))
+                model.Section = productDataService.GetSection(sectionId);
+
+            if (int.TryParse(Request.Query["BrandId"].ToString(), out var brandId))
+                model.Brand = productDataService.GetBrand(brandId);
+
+            if (int.TryParse(ViewContext.RouteData.Values["id"]?.ToString(), out var productId))
+            {
+                var product = productDataService.GetProductById(productId);
+                if (product != null) model.Product = product.Name;
+            }
+
+            return View(model);
+        }
+    }
+}
