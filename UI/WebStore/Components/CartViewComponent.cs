87a8ae49a145fdd998b66836c16f49e5203410b3
Tarxos <wolfmanr@mail.ru>
2020-07-14 23:24:08 +0300

    Cart View Component

---
+++ UI/WebStore/Components/CartViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.Components
+{
+    public class CartViewComponent : ViewComponent
+    {
+        private readonly ICartDataService cartService;
+
+        public CartViewComponent(ICartDataService cartService) => this.cartService = cartService;
+
+        public IViewComponentResult Invoke() => View(cartService.TransformFromCart());
+    }
+}
