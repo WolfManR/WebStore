b0c810fe9e4d58a02a1aac981f6d9574a244351b
Tarxos <wolfmanr@mail.ru>
2020-07-15 23:37:11 +0300

    pagination

--- Common/WebStore.Domain/ViewModels/PageViewModel.cs
+++ Common/WebStore.Domain/ViewModels/PageViewModel.cs
@@ -10,6 +10,14 @@ namespace WebStore.Domain.ViewModels
 
         public int PageNumber { get; set; }
 
-        public int TotalPages => (int)Math.Ceiling((decimal)TotalItems / PageSize);
+        public int TotalPages
+        {
+            get
+            {
+                var integer = (int) Math.Ceiling((decimal) TotalItems / PageSize);
+                var module = (int)Math.Ceiling((decimal)TotalItems % PageSize);
+                return module < PageSize ? integer+1 : integer;
+            }
+        }
     }
 }
