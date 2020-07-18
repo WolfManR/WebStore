2277b713a785ce7affc3314146f3423d45ed0bfd
Tarxos <wolfmanr@mail.ru>
2020-07-18 12:50:18 +0300

    Ajax Blank for play

---
+++ Common/WebStore.Domain/ViewModels/AjaxTestViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WebStore.Domain.ViewModels
+{
+    public class AjaxTestViewModel
+    {
+        public int Id { get; set; }
+        public string Message { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
