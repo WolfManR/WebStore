f85752bfc06b59b67c1ed43fe21bb566e62d28d5
Tarxos <wolfmanr@mail.ru>
2020-07-18 11:42:37 +0300

    Products async pagination

--- UI/WebStore/TagHelpers/PagingTagHelper.cs
+++ UI/WebStore/TagHelpers/PagingTagHelper.cs
@@ -1,11 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.AspNetCore.Mvc.Routing;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 using WebStore.Domain.ViewModels;
 
@@ -13,11 +11,6 @@ namespace WebStore.TagHelpers
 {
     public class PagingTagHelper : TagHelper
     {
-        private readonly IUrlHelperFactory urlHelperFactory;
-
-        [ViewContext, HtmlAttributeNotBound]
-        public ViewContext ViewContext { get; set; }
-
         public PageViewModel PageModel { get; set; }
 
         public string PageAction { get; set; }
@@ -25,31 +18,34 @@ namespace WebStore.TagHelpers
         [HtmlAttributeName(DictionaryAttributePrefix = "page-url-")]
         public Dictionary<string, object> PageUrlValues { get; set; } = new Dictionary<string, object>(StringComparer.OrdinalIgnoreCase);
 
-        public PagingTagHelper(IUrlHelperFactory urlHelperFactory) => this.urlHelperFactory = urlHelperFactory;
-
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             var ul = new TagBuilder("ul");
             ul.AddCssClass("pagination");
 
-            var urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
             for (int i = 1, totalPages = PageModel.TotalPages; i < totalPages; i++)
-                ul.InnerHtml.AppendHtml(CreateItem(i, urlHelper));
+                ul.InnerHtml.AppendHtml(CreateItem(i));
 
             output.Content.AppendHtml(ul);
         }
 
-        private TagBuilder CreateItem(int pageNumber, IUrlHelper url)
+        private TagBuilder CreateItem(int pageNumber)
         {
             var li = new TagBuilder("li");
             var a = new TagBuilder("a");
 
-            if (pageNumber == PageModel.PageNumber) li.AddCssClass("active");
+            if (pageNumber == PageModel.PageNumber)
+            {
+                a.MergeAttribute("data-page",PageModel.PageNumber.ToString());
+                li.AddCssClass("active");
+            }
             else
             {
                 PageUrlValues["page"] = pageNumber;
-                a.Attributes["href"] = url.Action(PageAction, PageUrlValues);
+                a.Attributes["href"] = "#";
+                foreach (var (key, value) in PageUrlValues.Where(v => v.Value != null))
+                    a.MergeAttribute($"data-{key}", value.ToString());
             }
 
             a.InnerHtml.AppendHtml(pageNumber.ToString());
